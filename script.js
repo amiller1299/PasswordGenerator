d8302f8abda9b9e7e5a495b78af744a1cdb617ec
amiller1299 <64334092+amiller1299@users.noreply.github.com>
2020-06-30 19:49:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = '0123456789';
+var symbols = '!@#$%^&*=-_';
 
+var lowerCaseNum = document.getElementById("lowerCaseNum");
+var upperCaseBox = document.getElementById("upperCase");
+var numbersBox = document.getElementById("numbers");
+var symbolsBox = document.getElementById("symbols");
+var submit = document.getElementById("submit");
+var yourPw = document.getElementById("yourPw");
+
+submit.addEventListener("click",function(e){
+    var characters = lowerCase;
+    (upperCasebox.checked) ? characters += upperCase : '';
+    (numbersBox.checked) ? characters += numbers : '';
+    (symbolsBox.checked) ? characters += symbols : '';
+    yourPw.value = password(lowerCaseNum.value, characters);
+});
+
+
+function password(l,characters){
+		var passWord = '';
+    for(var i = 0; i<l; i++){
+    		passWord += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return passWord;
+}
